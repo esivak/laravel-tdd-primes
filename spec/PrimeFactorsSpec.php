b9fac195697175d495944a9f335ccef269d246f3
Elanganathapillai Sivakanthan <siva@globalcitymedia.com>
2018-01-11 15:09:40 +0000

    Prime function working  no bugs

--- spec/PrimeFactorsSpec.php
+++ spec/PrimeFactorsSpec.php
@@ -17,4 +17,25 @@ class PrimeFactorsSpec extends ObjectBehavior
     function it_returns_an_empty_array_for1(){
         $this->generate(1)->shouldReturn([]);
     }
+
+    function it_returns_2_for2(){
+        $this->generate(2)->shouldReturn([2]);
+    }
+
+    function it_returns_3_for3(){
+        $this->generate(3)->shouldReturn([3]);
+    }
+
+    function it_returns_2_2_for4(){
+        $this->generate(4)->shouldReturn([2,2]);
+    }
+
+    function it_returns_2_2_5_5_for100(){
+        $this->generate(100)->shouldReturn([2,2,5,5]);
+    }
+
+    function it_returns7for7(){
+        $this->generate(7)->shouldReturn([7]);
+    }
+
 }
--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -2,10 +2,32 @@
 
 class PrimeFactors
 {
-    public function generate($argument1)
+    public function generate($num)
     {
         // TODO: write logic here
-        return [];
+
+        $primes = [];
+
+        while ($num%2==0){
+            $primes[] = 2;
+            $num /=2;
+        }
+
+        for ($i = 3; $i <= sqrt($num); $i+= 2)
+        {
+            // While i divides n, print i and divide n
+            while ($num%$i == 0)
+            {
+                $primes[] = $i;
+                $num/= $i;
+            }
+
+        }
+        if ($num > 2){
+            $primes[] = $num;
+        }
+
+        return $primes;
 
     }
 }
